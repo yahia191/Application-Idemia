26812a972a8c1fdb4eeb8f60ee0a70e1cace2710
Yahia Ait yaich <yahiaaityaich@MacBook-Air-de-Yahia.local>
2020-09-14 04:53:45 +0100

    vf avec statistique statique

--- src/app/views/chartjs/chartjs.component.ts
+++ src/app/views/chartjs/chartjs.component.ts
@@ -8,6 +8,124 @@ import { VariableAst } from "@angular/compiler";
   templateUrl: "chartjs.component.html",
 })
 export class ChartJSComponent implements OnInit {
+  /*  */
+  // lineChart
+  public lineChartData: Array<any> = [
+    { data: [74, 74, 74, 74, 0, 0], label: "Tests MTV2" },
+  ];
+  public lineChartLabels: Array<any> = [
+    "20200912",
+    "20200912",
+    "20200913",
+    "20200913",
+    "20200914",
+    "20200914",
+  ];
+  public lineChartOptions: any = {
+    animation: false,
+    responsive: true,
+  };
+  public lineChartColours: Array<any> = [
+    {
+      // grey
+      backgroundColor: "rgba(148,159,177,0.2)",
+      borderColor: "rgba(148,159,177,1)",
+      pointBackgroundColor: "rgba(148,159,177,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(148,159,177,0.8)",
+    },
+  ];
+
+  // lineChart2
+  public lineChartData2: Array<any> = [
+    { data: [180, 180, 180, 180, 0, 0], label: "Tests MR2250" },
+  ];
+  public lineChartLabels2: Array<any> = [
+    "20200912",
+    "20200912",
+    "20200913",
+    "20200913",
+    "20200914",
+    "20200914",
+  ];
+  public lineChartOptions2: any = {
+    animation: false,
+    responsive: true,
+  };
+  public lineChartColours2: Array<any> = [
+    {
+      // grey
+      backgroundColor: "rgba(148,159,177,0.2)",
+      borderColor: "rgba(148,159,177,1)",
+      pointBackgroundColor: "rgba(148,159,177,1)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgba(148,159,177,0.8)",
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = "line";
+
+  // barChart1
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+  public barChartLabels: string[] = [
+    "20200912",
+    "20200912",
+    "20200913",
+    "20200913",
+    "20200914",
+    "20200914",
+  ];
+  public barChartType = "bar";
+  public barChartLegend = true;
+
+  public barChartData: any[] = [
+    {
+      data: [42, 42, 42, 42, 0, 0],
+      label: "Test PASS",
+      backgroundColor: "rgba(75, 192, 192, 0.8)",
+    },
+    {
+      data: [32, 32, 32, 32, 0, 0],
+      label: "Test FAIL",
+      backgroundColor: "rgba(255, 99, 132, 0.8)",
+    },
+  ];
+
+  // barChart2
+  public barChartOptions2: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+  public barChartLabels2: string[] = [
+    "20200912",
+    "20200912",
+    "20200913",
+    "20200913",
+    "20200914",
+    "20200914",
+  ];
+  public barChartType2 = "bar";
+  public barChartLegend2 = true;
+
+  public barChartData2: any[] = [
+    {
+      data: [167, 167, 167, 167, 0, 0],
+      label: "Test PASS",
+      backgroundColor: "rgba(75, 192, 192, 0.8)",
+    },
+    {
+      data: [13, 13, 13, 13, 0, 0],
+      label: "Test FAIL",
+      backgroundColor: "rgba(255, 99, 132, 0.8)",
+    },
+  ];
+  /*  */
+
   type: any;
   infoSuccess: any;
   infoFail: any;
